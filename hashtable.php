24f85991028056fa0661234dad9746a0bc8a6b9b
Lachlan Ross <lwross@gmail.com>
2013-12-03 23:23:27 +1100

    Initial functions
    Should turn this into a class and protect $hashtable and $keys

---
+++ hashtable.php
@@ -0,0 +1,105 @@
+<?php
+define("HASHTABLESIZE",3);
+
+$hashtable = array_fill(0,HASHTABLESIZE,null);
+$keys = array_fill(0,HASHTABLESIZE,array());
+
+function hashfunc($key) {
+	$hash = 5381;
+
+	for ($i = 0; $i < count($key); $i++) {
+		$hash = (($hash << 5) + $hash) + ord($key[$i]);
+	}
+
+	return $hash % HASHTABLESIZE;
+}
+
+function set($key, $value) {
+	global $hashtable;
+	global $keys;
+	$bucket = hashfunc($key);
+	
+	if ($hashtable[$bucket] === null) {
+		$hashtable[$bucket] = $value;
+		$keys[$bucket][] = $key;
+	} else {
+		// collision
+
+		if (is_array($hashtable[$bucket])) {
+
+			$found = false;
+			foreach ($keys[$bucket] as $k => $v) {
+				if ($v == $key) {
+					$hashtable[$bucket][$k] = $value;
+					$found = true;
+					break;
+				}
+			}   
+
+			if (!$found) {
+				$hashtable[$bucket][] = $value;
+			}
+		} else {
+			if ($keys[$bucket][0] == $key) {
+				$hashtable[$bucket] = $value;
+			} else {
+				$hashtable[$bucket] = array($hashtable[$bucket]);
+				$hashtable[$bucket][] = $value;
+			}
+		}	
+		if (!in_array($key, $keys[$bucket])) {
+			$keys[$bucket][] = $key;
+		}
+
+	}
+}
+
+function get($key) {
+	global $hashtable;
+	global $keys;
+	$rv = false;
+	$bucket = hashfunc($key);
+	if ($value = $hashtable[$bucket]) {
+		if (is_array($value)) {
+			foreach ($keys[$bucket] as $k => $v) {
+				if ($v == $key) {
+					$rv = $value[$k];
+					break;
+				}
+			}	
+		} else {
+			$rv = $value;
+		}
+	}
+	return $rv;
+}
+
+function delete($key) {
+
+	global $hashtable;
+	global $keys;
+	$rv = false;
+	$bucket = hashfunc($key);
+
+	if ($hashtable[$bucket] !== null) {
+		if (is_array($hashtable[$bucket])) {
+			foreach ($keys[$bucket] as $k => $v) {
+				if ($v == $key) {
+					unset($hashtable[$bucket][$k]);
+					unset($keys[$bucket][$k]);
+					break;
+				}
+			}	
+			if (count($hashtable[$bucket]) < 1) {
+				$hashtable[$bucket] = null;
+				$keys[$bucket] = array();
+			}
+		} else {
+			$hashtable[$bucket] = null;
+			$keys[$bucket] = array();
+		}
+	}
+	return $rv;
+}
+
+?>
